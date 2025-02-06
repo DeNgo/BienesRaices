1995b061ea76d535d2f885d0db3231ef2c1ad437
DeNgo <ruannicolas_@hotmail.com>
2025-02-06 13:15:05 +0100

    agregando conexión a la base de datos

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,1 +1,11 @@
- 
+<?php
+
+function conectarDB() : mysqli {
+    $db = mysqli_connect('localhost', 'root', 'Vasco2001', 'bienesraices_crud');
+
+    if (!$db) {
+        echo 'Error no se pudo conectar';
+        exit;
+    }
+    return $db;
+}
