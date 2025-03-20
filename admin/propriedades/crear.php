f24424dabf19a56a5892aecbcab081a98f776895
DeNgo <ruannicolas_@hotmail.com>
2025-03-20 19:36:24 +0100

    escapando entradas de formulario en crear.php para mejorar la seguridad

--- admin/propriedades/crear.php
+++ admin/propriedades/crear.php
@@ -21,14 +21,14 @@ $creado = date('Y-m-d');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-
-    $titulo        = $_POST['titulo'];
-    $precio        = $_POST['precio'];
-    $descripcion   = $_POST['descripcion'];
-    $habitaciones  = $_POST['habitaciones'];
-    $wc            = $_POST['wc'];
-    $estacionamiento = $_POST['estacionamiento'];
-    $vendedorId    = $_POST['vendedorId'];
+    $titulo        = mysqli_real_escape_string($db, $_POST['titulo']);
+    $precio        = mysqli_real_escape_string($db, $_POST['precio']);
+    $descripcion   = mysqli_real_escape_string($db, $_POST['descripcion']);
+    $habitaciones  = mysqli_real_escape_string($db, $_POST['habitaciones']);
+    $wc            = mysqli_real_escape_string($db, $_POST['wc']);
+    $estacionamiento = mysqli_real_escape_string($db, $_POST['estacionamiento']);
+    $vendedorId    = mysqli_real_escape_string($db, $_POST['vendedorId']);
+    $creado = date('Y-m-d');
 
     if (!$titulo) {
         $errores[] = "Debes añadir un título";
