ac7a79cb6b5b190adedc4d7453edb17bac3a04ff
DeNgo <ruannicolas_@hotmail.com>
2025-03-31 10:01:40 +0200

    agregando mensajes de éxito en la creación de propiedades y mejorando el estilo de alerta

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,15 @@
 <?php
+
+$resultado = $_GET['resultado'] ?? null;
 require '../includes/funciones.php';
 incluirTemplate('header');
 ?>
 
 <main class="contenedor seccion">
     <h1>Administrador de Bienes Raices</h1>
+    <?php if ($resultado == '1') : ?>
+        <p class="alerta success">Propriedad Creada Correctamente</p>
+    <?php endif; ?>
 
     <a href="/admin/propriedades/crear.php" class="boton boton-verde">Nueva Propriedad</a>
 </main>
