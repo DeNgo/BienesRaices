de6fc73ff91e2376056d3b95a870be4a40ed5d89
DeNgo <ruannicolas_@hotmail.com>
2025-03-31 21:38:53 +0200

    agregando tabla de propiedades en el panel de administración y estilos para botones de acción

--- admin/index.php
+++ admin/index.php
@@ -12,6 +12,31 @@ incluirTemplate('header');
     <?php endif; ?>
 
     <a href="/admin/propriedades/crear.php" class="boton boton-verde">Nueva Propriedad</a>
+
+    <table class="propriedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Casa en la Playa</td>
+                <td><img src="../imagenes/36a36158f1eec067e8925f1c908d511a.jpg" class="imagen-tabla"></td>
+                <td>120000€</td>
+                <td>
+                    <a href="#" class="boton-rojo-block">Eliminar</a>
+                    <a href="#" class="boton-amarillo-block">Actualizar</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </main>
 
 <?php
