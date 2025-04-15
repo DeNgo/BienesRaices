bad3027709ff207377266a84133a8e4c8bee3e92
DeNgo <ruannicolas_@hotmail.com>
2025-04-15 11:11:36 +0200

    agregando funcionalidad para mostrar propiedades desde la base de datos y mejorando la estructura de anuncios

--- index.php
+++ index.php
@@ -36,101 +36,12 @@ incluirTemplate('header', $inicio);
 </main>
 <section class="seccion contenedor">
     <h2>Casas y Apartamentos en Venta</h2>
-    <div class="contenedor-anuncios">
-        <div class="anuncio">
-            <picture>
-                <source srcset="build/img/anuncio1.webp" type="image/webp">
-                <source srcset="build/img/anuncio1.jpg" type="image/jpeg">
-                <img loading="lazy" src="build/img/anuncio1.jpg" alt="anuncio">
-            </picture>
+    
+    <?php
+        $limit = 3;
+        include 'includes/templates/anuncios.php';
+    ?>
 
-            <div class="contenido-anuncio">
-                <h3>Casa de lujo en el lago</h3>
-                <p>Casa en el lago con excelente vista, y acabados de lujo</p>
-                <p class="precio">3.000.000€</p>
-
-                <ul class="iconos-caracteristicas">
-                    <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitacion">
-                        <p>3</p>
-                    </li>
-                    <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono WC">
-                        <p>4</p>
-                    </li>
-                    <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono garaje">
-                        <p>3</p>
-                    </li>
-                </ul>
-                <a href="anuncios.php" class="boton-amarillo-block">
-                    Ver Propriedad
-                </a>
-            </div>
-        </div>
-        <div class="anuncio">
-            <picture>
-                <source srcset="build/img/anuncio2.webp" type="image/webp">
-                <source srcset="build/img/anuncio2.jpg" type="image/jpeg">
-                <img loading="lazy" src="build/img/anuncio2.jpg" alt="anuncio">
-            </picture>
-
-            <div class="contenido-anuncio">
-                <h3>Mansion Moderna</h3>
-                <p>Mansion con diseño moderno, amueblada</p>
-                <p class="precio">1.536.000€</p>
-
-                <ul class="iconos-caracteristicas">
-                    <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitacion">
-                        <p>4</p>
-                    </li>
-                    <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono WC">
-                        <p>3</p>
-                    </li>
-                    <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono garaje">
-                        <p>2</p>
-                    </li>
-                </ul>
-                <a href="anuncios.php" class="boton-amarillo-block">
-                    Ver Propriedad
-                </a>
-            </div>
-        </div>
-        <div class="anuncio">
-            <picture>
-                <source srcset="build/img/anuncio3.webp" type="image/webp">
-                <source srcset="build/img/anuncio3.jpg" type="image/jpeg">
-                <img loading="lazy" src="build/img/anuncio3.jpg" alt="anuncio">
-            </picture>
-
-            <div class="contenido-anuncio">
-                <h3>Casa con Piscina</h3>
-                <p>Casa en el lago con excelente vista, y acabados de lujo</p>
-                <p class="precio">3.000.000€</p>
-
-                <ul class="iconos-caracteristicas">
-                    <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitacion">
-                        <p>5</p>
-                    </li>
-                    <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono WC">
-                        <p>5</p>
-                    </li>
-                    <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono garaje">
-                        <p>3</p>
-                    </li>
-                </ul>
-                <a href="anuncios.php" class="boton-amarillo-block">
-                    Ver Propriedad
-                </a>
-            </div>
-        </div>
-    </div>
     <div class="alinear-derecha">
         <a href="anuncios.php" class="boton-verde">Ver Todas</a>
     </div>
